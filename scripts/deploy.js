dcd6649c8e3cf62855676eb179e51f5b079cbad6
yuk6ra <yuki.imamura.all@gmail.com>
2023-06-18 13:43:11 +0900

    Add deploy file for SqairAccountFactory

---
+++ scripts/deploy.js
@@ -0,0 +1,34 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require('hardhat')
+
+async function main () {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+
+  const entryPoint = '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789'
+  const Singleton = await hre.ethers.getContractFactory('SqairAccountFactory')
+  const singleton = await Singleton.deploy(entryPoint)
+
+  await singleton.deployed()
+
+  console.log('SqairAccountFactory deployed to:', singleton.address)
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
